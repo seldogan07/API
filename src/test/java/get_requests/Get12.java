c1ca9d9af396e3dd14e07e98f752af382fd54571
Selçuk Doğan <seldogan07@gmail.com>
2023-04-06 18:28:38 +0300

    adding  API 06.04

--- src/test/java/get_requests/Get12.java
+++ src/test/java/get_requests/Get12.java
@@ -1,12 +1,16 @@
 package get_requests;
 
 import base_urls.GoRestBaseUrl;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
 
 public class Get12 extends GoRestBaseUrl {
     /*
@@ -44,8 +48,27 @@ public class Get12 extends GoRestBaseUrl {
                 statusCode(200).
                 body("meta.pagination.limit",equalTo(10),
                         "meta.pagination.links.current",equalTo("https://gorest.co.in/public/v1/users?page=1"),
-                        "data",hasSize(10),"data.status",hasItem("active"),"data.name",hasItems("Marut Nambeesan DC","Ms. Ankal Sinha","Chaturbhuj Adiga"));
+                        "data",hasSize(10),"data.status",hasItem("active"),"data.name",hasItems("Sushma Somayaji","Girika Dwivedi","Anjali Sharma III"));
+        //The female users are less than or equals to male users
+        //1st way
+        JsonPath jsonpPath =response.jsonPath();
+        List<String> allGenders = jsonpPath.getList("data.gender");
+        System.out.println("allGenders = " + allGenders);
+        int numberOfFemale=0;
+        for (String w:allGenders){
+            if(w.equals("female")){
+                numberOfFemale++;
+            }
+        }
+        System.out.println("numberOfFemale = " + numberOfFemale);
+        assertTrue(numberOfFemale<=allGenders.size()-numberOfFemale);
 
+        //2nd way
+        int numberOfFemaleUser= jsonpPath.getList("data.findAll{it.gender=='female'}").size();
+        System.out.println("numberOfFemaleUser = " + numberOfFemaleUser);
+        int numberOfMaleUser= jsonpPath.getList("data.findAll{it.gender=='male'}").size();
+        System.out.println("numberOfMaleUser = " + numberOfMaleUser);
+        assertTrue(numberOfFemaleUser<=numberOfMaleUser);
 
     }
 
